a9281a460dee20d1bfbbade9f94054e18ba5c421
farkuy <darkuy@mail.ru>
2024-07-30 21:24:10 +0300

    в вебпак добавлен лоадер для scss

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 export default (env: BuildEnv) => {
     const paths: BuildPaths = {
-        entry: path.resolve(__dirname, 'src', 'index.ts'), // entry - стартовая точка приложения
+        entry: path.resolve(__dirname, 'src', 'index.tsx'), // entry - стартовая точка приложения
         build: path.resolve(__dirname, "build"), // путь куда сборка будет происходить,
         html: path.resolve(__dirname, 'public', 'index.html'), // Путь на основе чего собираем
     }
