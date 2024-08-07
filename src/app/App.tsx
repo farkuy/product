fb4c930465ba1ab096756da87bf8ea82843ddc22
farkuy <darkuy@mail.ru>
2024-08-07 23:15:36 +0300

    Добавлен виджет LangSwitcher

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,24 +4,7 @@ import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
 import {Navbar, SideBar} from "widgets";
-import {useTranslation} from "react-i18next";
-
-
-const Component = () => {
-    const { t, i18n } = useTranslation();
-
-    const toggle = () => {
-        i18n.changeLanguage(i18n.language === "ru" ? "en" : "ru")
-    }
-
-    return (
-        <div>
-            <button onClick={toggle}>{t("Перевод")}</button>
-            {t("Тестовый перевод")}
-        </div>
-    )
-}
-
+import {LangSwitcher} from "widgets/LangSwitcher/ui/LangSwitcher";
 
 const App = () => {
    const { theme } = useTheme();
@@ -30,7 +13,6 @@ const App = () => {
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar/>
-                <Component/>
                 <div className={'content-page'}>
                     <SideBar/>
                     <AppRouter/>
--- src/widgets/SideBar/ui/SideBar.tsx
+++ src/widgets/SideBar/ui/SideBar.tsx
@@ -1,7 +1,8 @@
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from "./SideBar.module.scss"
-import {FC, useState} from "react";
+import React, {FC, useState} from "react";
 import {ThemeSwitcher} from "widgets";
+import {LangSwitcher} from "widgets/LangSwitcher/ui/LangSwitcher";
 
 interface SideBarProps {
     className?: string;
@@ -18,9 +19,9 @@ export const SideBar:FC<SideBarProps> = (props) => {
     return (
         <div className={classNames(cls.SideBar, {[cls.collapsed]: collapsed}, [className])}>
             <button onClick={onToggle}>toggle</button>
+            <LangSwitcher/>
             <div className={cls.themeSwitcher}>
                 <ThemeSwitcher/>
-                {/*LangSwitcher сделать*/}
             </div>
         </div>
     );
