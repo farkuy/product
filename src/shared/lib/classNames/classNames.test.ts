4dac71f823fa6b7cf0899478055040d71c8d6a8c
farkuy <darkuy@mail.ru>
2024-08-14 22:36:12 +0300

    Первый тест

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,7 +1,23 @@
 import { classNames } from './classNames';
 
 describe('classnames', () => {
-    test('test', () => {
+    test('с единственным параметром', () => {
         expect(classNames('someClass')).toBe('someClass');
+    });
+
+    test('c доп классами', () => {
+        expect(classNames('someClass', {}, ['classOne', 'classTwo'])).toBe('someClass classOne classTwo');
+    });
+
+    test('c модом', () => {
+        expect(classNames('someClass', { hovered: true })).toBe('someClass hovered');
+    });
+
+    test('c 1 false модом', () => {
+        expect(classNames('someClass', { hovered: true, absolute: false, fixed: true })).toBe('someClass hovered fixed');
+    });
+
+    test('c 1 undefined модом', () => {
+        expect(classNames('someClass', { hovered: true, absolute: undefined, fixed: true })).toBe('someClass hovered fixed');
     });
 });
