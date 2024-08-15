5e5f2dc7593c5a8d105a5fc5b6afabec8638e30e
farkuy <darkuy@mail.ru>
2024-08-16 00:34:03 +0300

    Очистка ненужного

--- src/app/providers/ErrorBoundary/ui/BugButton.tsx
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -1,15 +1,9 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import { FC, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button } from 'shared/ui/Button/Button';
-import cls from './BugButton.module.scss';
 
-interface BugButtonProps {
-    className?: string;
-}
-
-export const BugButton:FC<BugButtonProps> = (props) => {
-    const { className } = props;
+// Тестовый компонет
+export const BugButton = () => {
     const { t } = useTranslation();
 
     const [error, setError] = useState(false);
@@ -17,12 +11,11 @@ export const BugButton:FC<BugButtonProps> = (props) => {
     const throwError = () => setError(true);
 
     useEffect(() => {
-        throw new Error();
+        if (error) throw new Error();
     }, [error]);
 
     return (
         <Button
-            className={classNames(cls.BugButton, {}, [className])}
             onClick={throwError}
         >
             {t('Выбросить ошибку')}
